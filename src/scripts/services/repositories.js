ea9b64123c41a9c6f3599bac19a33651ad8f3b46
Alexandre Federice <alexandre.federice@outlook.com>
2024-11-19 17:00:53 -0300

    Adicionado objects.js e screen.js, separando assim as responsabilidades. Código refatorado

--- src/scripts/services/repositories.js
+++ src/scripts/services/repositories.js
@@ -1,9 +1,9 @@
 import { baseUrl, repositoriesQuantity } from "/src/scripts/variables.js"
 
 // Função assíncrona para chamar o EndPoint da API do GitHub dos repositórios do usuário.
-async function repositories(userName) {
+async function getRepositories(userName) {
     const response = await fetch(`${baseUrl}/${userName}/repos?per_page=${repositoriesQuantity}`);
     return await response.json()
 }
 
-export { repositories }
+export { getRepositories }
--- src/scripts/services/user.js
+++ src/scripts/services/user.js
@@ -1,9 +1,9 @@
 import { baseUrl } from "/src/scripts/variables.js"
 
 // Função assíncrona para chamar o EndPoint da API do GitHub de nome do usuário.
-async function user(userName) {
+async function getUser(userName) {
     const response = await fetch(`${baseUrl}/${userName}`);
     return await response.json()
 }
 
-export { user }
+export { getUser }

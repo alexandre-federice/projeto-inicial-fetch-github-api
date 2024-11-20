39100f2bed0cb8ec0f45dffbe612af2b85c05d7f
Alexandre Federice <alexandre.federice@outlook.com>
2024-11-20 11:29:48 -0300

    Adicionado tratamento de erros

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -13,6 +13,7 @@ import { screen } from "/src/scripts/objects/screen.js"
 // Coloca um ouvinte no botão e chama a função getUserData quando o mesmo é clicado.
 document.getElementById("btn-search").addEventListener("click", () => {
     const userName = document.getElementById("input-search").value
+    if (validateEmptyInput(userName)) return
     getUserData(userName)
 })
 
@@ -22,14 +23,29 @@ document.getElementById("input-search").addEventListener("keyup", (e) => {
     const key = e.wich || e.keyCode
     const isEnterKeyPressed = key === 13
     if (isEnterKeyPressed) {
+        if (validateEmptyInput(userName)) return
         getUserData(userName)
     }
 })
+
+// Função para validar se o campo for vazio. Se sim, irá retornar uma mensagem para o usuário de obrigatoriedade de preenchimento
+function validateEmptyInput(userName) {
+    if (userName.length === 0) {
+        alert("Preencha o campo com o nome do usuário do GitHub")
+        return true
+    }
+}
 
 // Função para gerenciar as chamadas e interações, e renderizar os dados no DOM.
 async function getUserData(userName) {
 
     const userResponse = await getUser(userName)
+
+    if (userResponse.message === "Not Found") {
+        screen.renderNotFound()
+        return
+    }
+
     const repositoriesResponse = await getRepositories(userName)
 
     user.setInfo(userResponse)
